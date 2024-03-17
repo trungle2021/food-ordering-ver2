f250bb7325c4d6984c61959b87ca5cf1a552da50
trungle2021 <leanhtrung97@gmail.com>
2024-03-17 10:13:15 +0700

    .

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -30,11 +30,17 @@ instance.interceptors.request.use(
     }
 )
 
-instance.interceptors.response.use(config => {
 
-}, error => {
 
-})
+instance.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response.data;
+  }, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  })
 
 
 
